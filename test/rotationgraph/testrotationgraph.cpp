1c235ed43586fadd5de44066865086ff6b060816
filipfur <filipfur@live.se>
2023-04-30 11:57:02 +0200

    unit test for rotationgraph

---
+++ test/rotationgraph/testrotationgraph.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+int main(int argc, const char* argv[])
+{
+    glm::vec3 v{1.0f, 0.0f, 0.0f};
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
